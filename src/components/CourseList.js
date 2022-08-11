18181c4ecbf07e476c2b12ea619ba65e0f8b5956
Oliver Partida <oliver.partida@sage.com>
2022-08-11 16:11:11 +0200

    fix: add react icons

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,60 +1,91 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
-import { graphql, StaticQuery } from 'gatsby'
+import * as React from "react";
+import PropTypes from "prop-types";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import { graphql, Link, StaticQuery } from "gatsby";
+import { FaCalendarAlt, FaClock, FaHome } from "react-icons/fa";
 
 class CourseListTemplate extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div className="columns is-multiline">
-        {posts.map((item) => (
-          <div key={item.node.frontmatter.text} className="column is-6">
-            <section className="section box">
-              <div className="has-text-centered">
-                <h3>{item.node.frontmatter.title}</h3>
-                <div
-                  className="mb-4"
-                  style={{
-                    width: '100%',
-                    display: 'inline-block',
-                  }}
-                >
-                  <PreviewCompatibleImage
-                    imageInfo={item.node.frontmatter.featuredimage}
-                  />
-                </div>
-              </div>
-              <div className="panel-block">
-                <p>{item.node.frontmatter.description}</p>
-              </div>
-              <div className="panel-block">
-                <div className="list">
-                  <div className="list-item">
-                    <span className="has-text-weight-bold">Lugar: </span>
-                    {item.node.frontmatter.place}
-                  </div>
-                  <div className="list-item">
-                    <span className="has-text-weight-bold">Fechas: </span>
-                    {item.node.frontmatter.dates}
-                  </div>
-                  <div className="list-item">
-                    <span className="has-text-weight-bold">Duración: </span>
-                    {item.node.frontmatter.duration}
+        {posts.map((item) => {
+          return (
+            <div key={item.node.frontmatter.text} className="column is-6">
+              <section className="section box">
+                <div className="has-text-centered">
+                  <h3>{item.node.frontmatter.title}</h3>
+                  <div
+                    className="mb-4  mt-4"
+                    style={{
+                      width: "100%",
+                      display: "inline-block",
+                    }}
+                  >
+                    <PreviewCompatibleImage
+                      imageInfo={item.node.frontmatter.featuredimage}
+                    />
                   </div>
                 </div>
-              </div>
-              <div class="buttons is-centered mt-5">
-                <button class="button is-primary">Mas info</button>
-                <button class="button is-primary">Inscríbete</button>
-              </div>
-            </section>
-          </div>
-        ))}
+                <div className="mt-4">
+                  <p>{item.node.frontmatter.description}</p>
+                </div>
+                <div className="panel-block">
+                  <div className="list">
+                    <div className="list-item mb-4 mt-4">
+                      <span class="icon-text is-flex-wrap-nowrap">
+                        <span class="icon  mr-4">
+                          <FaHome />
+                        </span>
+                        <span className="has-text-weight-semibold">
+                          {item.node.frontmatter.place}
+                        </span>
+                      </span>
+                    </div>
+                    <div className="list-item mb-4">
+                      <span class="icon-text is-flex-wrap-nowrap">
+                        <span class="icon  mr-4">
+                          <FaCalendarAlt />
+                        </span>
+                        <span className="has-text-weight-semibold">
+                          {item.node.frontmatter.dates}
+                        </span>
+                      </span>
+                    </div>
+                    <div className="list-item mb-4">
+                      <span class="icon-text is-flex-wrap-nowrap">
+                        <span class="icon mr-4">
+                          <FaClock />
+                        </span>
+                        <span className="has-text-weight-semibold">
+                          {item.node.frontmatter.duration}
+                        </span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="buttons is-centered mt-5">
+                  <Link
+                    className="button is-primary"
+                    to={item.node.fields.slug}
+                  >
+                    Mas Info →
+                  </Link>
+                  <Link
+                    className="button is-primary"
+                    to={item.node.fields.slug}
+                  >
+                    Inscríbete
+                  </Link>
+                </div>
+              </section>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
@@ -64,7 +95,7 @@ CourseList.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default function CourseList() {
   return (
@@ -108,5 +139,5 @@ export default function CourseList() {
       `}
       render={(data, count) => <CourseListTemplate data={data} count={count} />}
     />
-  )
+  );
 }
